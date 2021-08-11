840f78658ce381a67ba7ec671224e0e84abf4995
konstantinreso <88545523+konstantinreso@users.noreply.github.com>
2021-08-11 14:42:52 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,22 @@ export default class App extends React.Component {
         <section className="main" style={{display: 'block'}}>
           <input id="toggle-all" className="toggle-all" type="checkbox" />
           <label htmlFor="toggle-all">Mark all as complete</label>
-          <ul className="todo-list"><li data-id={1628678522970} className><div className="view"><input className="toggle" type="checkbox" /><label>Note 1</label><button className="destroy" /></div></li><li data-id={1628678526196} className><div className="view"><input className="toggle" type="checkbox" /><label>Note 2</label><button className="destroy" /></div></li></ul>
+          <ul className="todo-list">
+            <li data-id={1628678522970} className>
+              <div className="view">
+                <input className="toggle" type="checkbox" />
+                <label>Note 1</label>
+                <button className="destroy" />
+              </div>
+            </li>
+            <li data-id={1628678526196} className>
+              <div className="view">
+                <input className="toggle" type="checkbox" />
+                <label>Note 2</label>
+                <button className="destroy" />
+              </div>
+            </li>
+          </ul>
         </section>
         <footer className="footer" style={{display: 'block'}}>
           <span className="todo-count"><strong>2</strong> items left</span>
